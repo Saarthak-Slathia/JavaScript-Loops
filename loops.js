aa75b768ba0929c2a714b545613bab34d4d51a49
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-01-24 14:49:21 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,56 @@
+// Welcome to JavaScript Loops !!!
+
+// There are mainly 3 types of loops in JavaScript:-
+// 1. for loop
+// 2. while loop
+// 3. do-while loop
+
+// 1. for loop
+for (let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+// 2. while loop
+let i = 0; // Defining the variable i with the value 0.
+while (i <= 10){  // Condition that if i is less than or equal to 10 then:-
+    console.log(i); // Prints the value of i in the console.
+    i++;  // Increments or increases the value of i by 1.
+}
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+// 3. do-while loop:
+let j = 0;
+do{ // Tells what to do.
+    console.log(j); // Printing the value of j in the console.
+    j++; // Incrementing the value of j by 1.
+} while(j <= 0); //Checking the condition.
+
+
+// Difference between while and do-while loop:
+// The while loop will not execute the statements if the condition is not true and will break the loop as the condition is checked first.
+// Whereas in the do-while loop, it first executes the statements and then checks the condition, so even if the condition is false, it will still execute the statements once.
+
+// Thank You !!!
